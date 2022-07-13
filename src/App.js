edcc48f5754b3e400b8e82b3f893eb2c122c8c08
maxamed <98mohamed.mo@gmail.com>
2022-07-14 00:26:36 +0100

    h1 text changed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-link">deploy using github v1</h1>
+        <h1 className="App-link">deploy using github v2</h1>
       </header>
     </div>
   );
